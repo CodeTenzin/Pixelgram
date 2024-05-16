072c5c0e6b785cbe5db2eb0866e65a3f37ab21d3
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-05-15 21:46:09 -0400

    Pixelgram

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+import "./globals.css";
+
+const App = () => {
+  return <main className="flex h-screen">Pixelgram</main>;
+};
+
+export default App;
